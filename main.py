58c3c72ccd9b82c87d850ca039a74042329eae75
abdullahzulfiqar785 <abdullahzulfiqar785@gmail.com>
2022-11-20 23:16:54 +0500

    main.py cleanup with simple calls of methods

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-import os
-import xlrd
-import openpyxl
 import pandas as pd
+from src import (
+    get_files,
+    get_files_with_same_headers,
+    )
 
 
-list_of_files = os.listdir('../samples')
-xlsx_files = list()
-csv_files = list()
-for filename in list_of_files:
-    if "xlsx" in filename:
-        xlsx_files.append(filename)
-    if 'csv' in filename:
-        csv_files.append(filename)
+files_directory_path = '../samples'
 
-
-for filename in xlsx_files:
-    address = "../samples/{}".format(filename)
-    wb_obj = openpyxl.load_workbook(address)
-    sheet_obj = wb_obj.active
-
-
-
-for i in range(sheet_obj.max_column):
-    print(sheet_obj.cell(row = 5, column = i+1).value)
-# print("filename: ", xlsx_files)
-# print("csv files:", csv_files)
+xlsx_files, csv_files = get_files(files_directory_path)
+files_dict_with_same_headers = get_files_with_same_headers(xlsx_files, files_directory_path)
