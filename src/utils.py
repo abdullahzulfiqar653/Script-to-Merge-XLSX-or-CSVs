e1fde5afd058cd3def3653c7679aaeeb29398581
abdullahzulfiqar785 <abdullahzulfiqar785@gmail.com>
2022-11-22 23:24:20 +0500

    os.walk() implemented and all the code updated accordingly

--- src/utils.py
+++ src/utils.py
@@ -8,28 +8,25 @@ def get_files(path):
         print(root)
         for filename in files:
             if filename.endswith('xlsx'):
-                xlsx_files.append("{}/{}".format(root, filename))
+                xlsx_files.append("{}\{}".format(str(root), filename))
             elif filename.endswith('csv'):
                 csv_files.append(filename)
-    print(xlsx_files, "-----")
     return xlsx_files, csv_files
 
-def get_files_with_same_headers(xlsx_files, files_directory):
+def get_files_with_same_headers(xlsx_files):
     columns_headers_sets = {}
-    for filename in xlsx_files:
-        headers_list, _ = get_headers_and_sheet(files_directory, filename)
+    for fileAddress in xlsx_files:
+        headers_list, _ = get_headers_and_sheet(fileAddress)
         name_of_key = str(headers_list)
         if name_of_key not in columns_headers_sets:
             columns_headers_sets[name_of_key] = list()
-        columns_headers_sets[name_of_key].append(filename)
+        columns_headers_sets[name_of_key].append(fileAddress)
     return list(columns_headers_sets.values())
 
-def get_headers_and_sheet(files_directory, filename):
-    address_to_file = "{}/{}".format(files_directory, filename)
-    wb_obj = openpyxl.load_workbook(address_to_file, read_only=True)
+def get_headers_and_sheet(fileAdress):
+    wb_obj = openpyxl.load_workbook(fileAdress, read_only=True)
     sheet_obj = wb_obj.active
     headers_list = list()
     for i in range(sheet_obj.max_column):
         headers_list.append(sheet_obj.cell(row = 5, column = i+1).value)
-        
     return headers_list, sheet_obj
