4dae0b371f4c6cc3fe419cdd469a421faa0de25d
abdullahzulfiqar785 <abdullahzulfiqar785@gmail.com>
2022-11-23 17:54:51 +0500

    Code cleanup

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ files_directory_path = '..\samples'
 
 xlsx_files, csv_files = get_files(files_directory_path)
 files_dict_with_same_headers = get_files_with_same_headers(xlsx_files)
-print(files_dict_with_same_headers)
+
 for listOfFileAdresses in files_dict_with_same_headers:
     if len(listOfFileAdresses) > 1:
         headers, header_row, sheet = get_headers_and_sheet(listOfFileAdresses[0])
